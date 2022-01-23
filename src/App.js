d4edac9904c0e3588dc083592b08e504abe6677a
yattew <yathansh@gmail.com>
2022-01-23 19:29:08 +0530

    dark mode toggle now working fully

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Test from "./components/test";
 
 
 function App() {
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light");
     useEffect(() => {
         let root = document.getElementById("root");
         if (theme === "light")
@@ -13,12 +13,20 @@ function App() {
         else if (theme === "dark")
             root.classList = "dark-mode";
     }, [theme]);
+    useEffect(() => {
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", e => {
+            if (e.matches)
+                setTheme("dark");
+            else
+                setTheme("light");
+        })
+    }, []);
     return (
         <>
             <Test />
             <div className="bottom-container">
                 <div>
-                    <BottomSelector theme={theme} setTheme={setTheme}/>
+                    <BottomSelector theme={theme} setTheme={setTheme} />
                 </div>
             </div>
         </>
