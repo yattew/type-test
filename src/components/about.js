16132aeb03838c6f3be7e20f66d7f23cf99e3958
yattew <yathansh@gmail.com>
2022-01-23 19:43:23 +0530

    added basic about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,20 @@
 import React from "react";
-function About(){
+function About() {
     return (
         <>
             <div className="bottom-box-container">
                 <div className="bottom-box-wrapper">
                     <div>
-                        this is a typing test
+                        This is a typing test
+                        <br />
+                        This app was developed using the React by Yathansh Tewatia
+                        <br />
+                        <a
+                            style={{ color: "inherit" }}
+                            href="https://github.com/yattew/type-test"
+                        >
+                            Github
+                        </a>
                     </div>
                 </div>
             </div>
